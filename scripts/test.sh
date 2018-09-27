943fe3840833d3955cac4e2c03a4eff779b212e5
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2018-09-26 22:13:06 -0400

    Try starting glxinfo with sudo

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,7 +11,7 @@ fi
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   sudo /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24
-  glxinfo
+  sudo glxinfo
 fi
 
 npm test
