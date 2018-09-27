606e3e801b0acd81d51567770a74c74559756761
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2018-09-26 22:04:17 -0400

    Try sudo for starting xvfb

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,7 +10,7 @@ else
 fi
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24
+  sudo /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24
   glxinfo
 fi
 
