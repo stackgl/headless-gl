ccf5c95bda8ba4c799c30c62555228eb33d994d1
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2018-09-26 22:25:58 -0400

    Try a different display server number

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,8 +10,8 @@ else
 fi
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-  sudo /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24
-  sudo glxinfo
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- ${DISPLAY} -ac -screen 0 1280x1024x24
+  glxinfo
 fi
 
 npm test
