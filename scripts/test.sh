88f2b51c18ec2548a94816a3ff5f7a0632f374df
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2018-09-26 21:52:48 -0400

    Another method of starting xfvb

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,7 +10,7 @@ else
 fi
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
-  sh -e /etc/init.d/xvfb start
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24
   glxinfo
 fi
 
