78ed3b6854056bc33bb896b3f9b2b9beb5a275d1
Daniel Hritzkiv <daniel.hritzkiv@gmail.com>
2018-09-26 22:43:36 -0400

    Enable tests on macOS

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,6 +12,6 @@ fi
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which glxinfo`
   xvfb-run --auto-servernum --server-num=1 -s "-ac -screen 0 1280x1024x24" `which npm` test
-#else
-#  npm test
+else
+  npm test
 fi
